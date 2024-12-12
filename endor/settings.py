baabb2f62770042936485c8a5255bc027babc365
gkipping2023 <gkipping01@gmail.com>
2024-12-12 18:14:58 -0500

    nov logbook

--- endor/settings.py
+++ endor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '123456789'#env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
